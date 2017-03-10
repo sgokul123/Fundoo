ae4b4f17fed1165b40740fb384dbe5536732a2ab
jyothishkm <jyothishkm88@gmail.com>
2017-03-10 11:46:44 +0530

    Fundoo Hr Changes

--- app/src/main/java/com/fundoohr/bridgeit/fundoohr/utility/MonthsData.java
+++
@@ -1,8 +0,0 @@
-/*package com.example.bridgeit.fundoo.utility;
-
-*//**
- * Created by bridgeit on 2/1/17.
- *//*
-public class MonthsData {
-    public static final String FIREBASEURL="https://fundoo-41e7c.firebaseio.com/";
-}*/
